f6d9f7d4b3805115a7c20bec18bd53506933fba3
SirCodesALoad <martyn.patrick@yahoo.co.uk>
2024-10-26 01:09:35 +0100

    Added Level Switch Mechanic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,9 @@ using Debug = UnityEngine.Debug;
 public class GameManager : MonoBehaviour
 {
     public List<Target> TargetSequence = new List<Target>{}, TargetsHit = new List<Target>{};
-    public int CurrentTargetIndex;
+    public int MaxNumberOfResets = 5, CurrentNumberOfResetsTriggered = 0;
+    public float DelayBetweenLevels = 0.5f;
+    private int CurrentTargetIndex, CurrentLeveLIndex;
 
     public void PlayerHitTarget(Target target)
     {
@@ -29,6 +31,8 @@ public class GameManager : MonoBehaviour
             }
             TargetsHit.Clear();
             Debug.Log("PLAYER HAS HIT INCORRECT TARGET!");
+            CurrentNumberOfResetsTriggered++;
+            OnPlayerResetTargets();
         }
         else
         {
@@ -40,7 +44,40 @@ public class GameManager : MonoBehaviour
             {
                 //Win! Move onto next stage.
                 Debug.Log("PLAYER HAS HIT ALL TARGETS IN SEQUENCE!");
+                StartCoroutine(EndLevel());
             }
+        }
+    }
+
+    void OnPlayerResetTargets()
+    {
+        if (CurrentNumberOfResetsTriggered >= MaxNumberOfResets)
+        {
+            var buildIndex = SceneManager.GetActiveScene().buildIndex - 1;
+            if (buildIndex > -1)
+            {
+                SceneManager.LoadScene(buildIndex);
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
+    IEnumerator EndLevel()
+    {
+        yield return new WaitForSeconds(DelayBetweenLevels);
+        var buildIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (buildIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(buildIndex);
+        }
+        else
+        {
+            //Player won!
+            Debug.Log("PLAYER HAS COMPLETED ALL SCENES");
+        
         }
     }
 
@@ -49,4 +86,5 @@ public class GameManager : MonoBehaviour
         var scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.name);
     }
+    
 }
